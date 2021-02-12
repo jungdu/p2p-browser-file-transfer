daccc86e7a75a9ee808b39e8eeb5872e27fc34af
fred <jungdujang@gmail.com>
2021-02-12 17:45:26 +0900

    Update: webpack 서버 빌드 설정 수정

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,13 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  target: "node",
   mode: "none",
   entry: './src/server/index.ts',
   devtool: 'inline-source-map',
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.ts$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
