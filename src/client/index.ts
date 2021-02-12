ce261cb60ccf29b3af41ff0733224fe788722efd
fred <jungdujang@gmail.com>
2021-02-13 02:50:01 +0900

    Update: client에서 파일 읽는 기능 추가

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,1 +1,40 @@
-console.log("Hello World")
+const nameIpt = document.getElementById("nameIpt") as HTMLInputElement;
+const fileInput = document.getElementById("fileInput") as HTMLInputElement;
+const uploadBtn = document.getElementById("uploadBtn") as HTMLButtonElement;
+
+uploadBtn.addEventListener("click", () => {
+  const fileToUpload = fileInput.files && fileInput.files[0];
+  if(fileToUpload){
+    readFile(fileToUpload, (blob) => {
+      console.log("readed blob :", blob)
+    })
+  }else{
+    console.error("No file input to upload");
+  }
+})
+
+
+function readFile(file:File, onGetChunk: (blob: ArrayBuffer) => void){
+  const chunkSize = 16384;
+  const fileReader = new FileReader();
+  let offset = 0;
+  fileReader.addEventListener('load', event => {
+    if(event && event.target && event.target.result instanceof ArrayBuffer){
+      onGetChunk(event.target.result);
+      offset += event.target.result.byteLength;
+      if (offset < file.size) {
+        readSlicedBlob(offset)
+      }
+    }
+  });
+  readSlicedBlob(0);
+
+  function readSlicedBlob(offset:number){
+    const slicedBlob = getSliceBlob(offset);
+    fileReader.readAsArrayBuffer(slicedBlob);
+  }
+
+  function getSliceBlob(offset:number){
+    return file.slice(offset, offset + chunkSize);
+  }
+}
